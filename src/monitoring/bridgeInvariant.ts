08ac29c53c1fe3eaa8fc1f4319917e4360951ff0
Nick Ulven <nickulven@gmail.com>
2022-08-11 13:06:50 -0700

    Fix format

--- src/monitoring/bridgeInvariant.ts
+++ src/monitoring/bridgeInvariant.ts
@@ -2,7 +2,7 @@ import { L1Sdk, L2Sdk } from '../sdks'
 
 export async function bridgeInvariant(l1Sdk: L1Sdk, l2Sdk: L2Sdk): Promise<{ l1Balance: string; l2Balance: string }> {
   const l1Balance = (await l1Sdk.dai.balanceOf(l1Sdk.escrow.address)).toString()
-  const l2Balance = ((await l2Sdk.dai.totalSupply()).res).toString()
+  const l2Balance = (await l2Sdk.dai.totalSupply()).res.toString()
 
   return {
     l1Balance,
